4d3f9da234b622d88ef85de3b05eea74376d5ce2
janina280 <coceijanina@yahoo.com>
2025-01-23 09:11:21 +0200

    fix bug

--- resume/src/components/common/Title.jsx
+++ resume/src/components/common/Title.jsx
@@ -1,11 +1,10 @@
 import PropTypes from "prop-types";
 
-const Title = (titleText) => {
+const Title = ({ titleText }) => {
   return (
     <div className="title flex intems-center">
-      <div className="dots-shape">
-        <h3>{titleText}</h3>
-      </div>
+      <div className="dots-shape"></div>
+      <h3>{titleText}</h3>
     </div>
   );
 };
