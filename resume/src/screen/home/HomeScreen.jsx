b59e0997a570dc1d8045346d14499990e78a8af2
janina280 <coceijanina@yahoo.com>
2025-01-23 10:28:09 +0200

    create education and experience

--- resume/src/screen/home/HomeScreen.jsx
+++ resume/src/screen/home/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import Cursor from "../../components/cursor/Cursor";
 import Education from "../../components/education/Education";
+import Experience from "../../components/experience/Experience";
 import Header from "../../components/header/Header";
 
 const Home = () => {
   return (
     <div className="page-container">
       <Header />
+      <Experience />
       <Education />
       <Cursor />
     </div>
